b3761012530551e33ac39a3d12e3042c65a743e1
SHON <elton.shon@keysight.com>
2016-10-11 03:36:32 -0700

    complete image building

--- map.py
+++ map.py
@@ -2,10 +2,9 @@ import numpy as np
 import dxfgrabber
 import cv2
 
-
 def main():
 	resolutionPower = 2
-	housingMaxLenInMM = 70
+	housingMaxLenInMM = 65
 	arrayLen = housingMaxLenInMM * 10 ** resolutionPower
 	XYoffset = (41.90 + 5, 28.25+ 5)
 	housing = np.ones((arrayLen ,arrayLen), np.uint8) * 255
@@ -14,31 +13,30 @@ def main():
 
 def drawPartsFromDxf(pixelArray, dxfFilePath, offset, resolution, arrayLength):
 	black = (0, 0, 0)
-	lineWidth = 5
-	pixelArray = cv2.circle(pixelArray, (7000,0), 500, black,-1)
-	pixelArray = cv2.circle(pixelArray, (7000,7000), 500, black,-1)
-
+	lineWidth = 3
 	dxf = dxfgrabber.readfile(dxfFilePath)
 	entity = dxf.entities
 	for part in entity:
-		if part.dxftype == 'LINE':
+		if part.dxftype == 'LINE':  # maybe need to add polyline
 			startP, endP = linePointsConversion(part.start, part.end, offset, resolution, arrayLength)
-			print(startP, endP)
 			pixelArray = cv2.line(pixelArray, startP, endP, black, lineWidth)
 			
 		if part.dxftype == 'ARC':
 			center = part.center
 			radius = part.radius
-			sAngle = part.start_angle
-			eAngle = part.end_angle
+			deltaAngle = abs(part.end_angle - part.start_angle)
+			if part.end_angle < part.start_angle:
+				deltaAngle = 360 - deltaAngle
+			sAngle = 360 - part.end_angle
+			eAngle = sAngle + deltaAngle
 			center, radius = circleConversion(center, radius, offset, resolution, arrayLength)
-			pixelArray = cv2.circle(pixelArray, center, radius, black, 5)
-		
+			pixelArray = cv2.ellipse(pixelArray, center, (radius, radius), 0, sAngle, eAngle, black, lineWidth)
+
 		if part.dxftype == 'CIRCLE':
 			center = part.center
 			radius = part.radius
 			center, radius = circleConversion(center, radius, offset, resolution, arrayLength)
-			pixelArray = cv2.circle(pixelArray, center, radius, black, 5)
+			pixelArray = cv2.circle(pixelArray, center, radius, black, lineWidth)
 
 	return pixelArray
 
@@ -52,47 +50,45 @@ def circleConversion(center, radius, offset, resolution, flip):
 	return center, radius
 
 def linePointsConversion(sPoint, ePoint, offset, resolution, flip):
-	sPointX = (sPoint[0] + offset[0]) * 10**resolution
-	sPointY = (sPoint[1] + offset[1]) * 10**resolution
-	ePointX = (ePoint[0] + offset[0]) * 10**resolution
-	ePointY = (ePoint[1] + offset[1]) * 10**resolution
-
+	sPointX = (sPoint[0] + offset[0]) * 10**(resolution+1)
+	sPointY = (sPoint[1] + offset[1]) * 10**(resolution+1)
+	ePointX = (ePoint[0] + offset[0]) * 10**(resolution+1)
+	ePointY = (ePoint[1] + offset[1]) * 10**(resolution+1)
 
 	if sPointX > ePointX:
 		if sPointX % 10 != 0:
-			sPointX = int(sPointX) + 1
+			sPointX = int(sPointX/10) + 1
 		else:
-			sPointX = int(sPointX)
-		ePointX = int(ePointX)
+			sPointX = int(sPointX/10) 
+		ePointX = int(ePointX/10)
 	else:
 		if ePointX % 10 != 0:
-			ePointX = int(ePointX) + 1
+			ePointX = int(ePointX/10)  + 1
 		else:
-			ePointX = int(ePointX)
-		sPointX = int(sPointX)
+			ePointX = int(ePointX/10) 
+		sPointX = int(sPointX/10)
 
 	if sPointY > ePointY:
 		if sPointY % 10 != 0:
-			sPointY = int(sPointY) + 1
+			sPointY = int(sPointY/10) + 1
 		else:
-			sPointY = int(sPointY)
-		ePointY = int(ePointY)
+			sPointY = int(sPointY/10) 
+		ePointY = int(ePointY/10) 
 	else:
 		if ePointY % 10 != 0:
-			ePointY = int(ePointY) + 1
+			ePointY = int(ePointY/10) + 1
 		else:
-			ePointY = int(ePointY)
-		sPointY = int(sPointY)
+			ePointY = int(ePointY/10)
+		sPointY = int(sPointY/10)
 
 	ePointY = flip - ePointY
 	sPointY = flip - sPointY
-
 	return (sPointX, sPointY), (ePointX, ePointY)
-
 
 def showImage(pixelArray):
 	cv2.namedWindow('window', cv2.WINDOW_KEEPRATIO)
 	cv2.imshow('window', pixelArray)
 	cv2.waitKey(0)
+	cv2.imwrite(r"C:\Users\eltoshon\Desktop\housingIMAGE.jpeg", pixelArray)
 
 main()
